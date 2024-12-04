0b1f3264742138951b2437d6f84a08399f491967
siddharthnathan <siddharthnathan@gmail.com>
2024-12-03 19:10:52 -0600

    Modified ur5_move.py

--- real_robot/TeleopMethods/ur5_move.py
+++ real_robot/TeleopMethods/ur5_move.py
@@ -20,6 +20,35 @@ def convert_into_radians(angles):
     return radians
 
 
+# Define a Function to Set Arms to Home Position
+def come_home_position(ur5):
+    
+    # For every Arm
+    for arm in arms:
+
+        # Goto Home position
+        ur5.URs.moveJ(arm, (convert_into_radians(home_angles[arm]), 0.5, 0.5))
+        
+        # Open Gripper
+        ur5.URs.get_gripper(arm).set(3)
+
+
+# Define a Function to Get Arms Poses wrt Base Frame
+def get_arms_poses(ur5):
+    
+    # Initialise Empty Dictionary to store Poses
+    poses = {}
+
+    # For every Arm
+    for arm in arms:
+        
+        # Get the Poses of Arms wrt Base
+        poses[arm] = ur5.URs.get_receive(arm).getActualTCPPose()
+
+    # Return Poses
+    return poses
+
+
 # Define a Function to Update Poses for given Arm
 def update_pose(pose, offset):
     
@@ -32,8 +61,23 @@ def update_pose(pose, offset):
     # Return Pose
     return pose
 
-    
 
+# Define a Function to Grasp at a given Location for all Arms
+def arms_grasp(ur5, poses, offsets):
+
+    # For every Arm
+    for arm in arms:
+
+        # Update Pose with Offsets
+        poses[arm] = update_pose(poses[arm], offsets[arm])
+
+        # Move Arms to Final Pose
+        ur5.URs.moveL(arm, (poses[arm], 0.02, 0.05, 0))
+        
+        # Close Gripper
+        ur5.URs.get_gripper(arm).set(255)
+
+  
 # Define the Main Function
 def main():
     
@@ -42,30 +86,19 @@ def main():
 
     # Come to Home Position
     print("Setting Home Position")
-    for arm in arms:
-        ur5.URs.moveJ(arm, (convert_into_radians(home_angles[arm]), 0.5, 0.5))
+    come_home_position(ur5)
 
     # Initialise Dictionary to store Poses wrt Base for all Arms
-    poses = {}
-    for arm in arms:
-        
-        # Get the Poses of Arms wrt Base
-        poses[arm] = ur5.URs.get_receive(arm).getActualTCPPose()
-    
+    poses = get_arms_poses(ur5)    
     
     offsets = {}
-    offsets['Thunder'] = [-0.1, 0, 0, 0, 0, 0]
-    offsets['Lightning'] = [0, 0.1, 0, 0, 0, 0]
-
-    # Update Poses for all Arms
-    for arm in arms:
-
-        # Update Pose with Offsets
-        poses[arm] = update_pose(poses[arm], offsets[arm])
-
-        # Move Arms to Final Pose
-        ur5.URs.moveL(arm, (poses[arm], 0.1, 0.1, 0))
+    offsets['Thunder'] = [0, 0, 0.1, 0, 0, 0]
+    offsets['Lightning'] = [0.1, 0, 0, 0, 0, 0]
     
+    # Move Arms to given Locations and Grasp
+    arms_grasp(ur5, poses, offsets)
+
+
 
 # Invoke Main Function
 if __name__ == '__main__':
